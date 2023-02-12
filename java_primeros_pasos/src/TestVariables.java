7dad1d8c80e23e028b0368092ca71e331cb077e9
jgnarvaez <jgnarvaez@unicauca.edu.co>
2023-02-11 21:06:40 -0500

    Test variable tipo double

--- java_primeros_pasos/src/TestVariables.java
+++ java_primeros_pasos/src/TestVariables.java
@@ -5,8 +5,16 @@ public class TestVariables {
 		System.out.println("Hola Mundo");
 		
 		int edad = 37;
-		
 		System.out.println("La edad es "+ edad + ", Felicitaciones!");
+		
+		double salario = 1250.70;
+        System.out.println ("mi salario es " + salario);
+        
+        int division = 5/2;
+        System.out.println (division);
+        
+        double division2 = 5.0 / 2;
+        System.out.println (division2);
 	}
 	
 }
