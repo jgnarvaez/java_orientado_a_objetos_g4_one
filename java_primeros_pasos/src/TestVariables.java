d6b7cf17cdc92e7375a8f7b77d15f1f0567b1c48
jgnarvaez <jgnarvaez@unicauca.edu.co>
2023-02-11 21:18:44 -0500

    Conversion

--- java_primeros_pasos/src/TestVariables.java
+++ java_primeros_pasos/src/TestVariables.java
@@ -6,15 +6,24 @@ public class TestVariables {
 		
 		int edad = 37;
 		System.out.println("La edad es "+ edad + ", Felicitaciones!");
-		
-		double salario = 1250.70;
-        System.out.println ("mi salario es " + salario);
         
         int division = 5/2;
         System.out.println (division);
         
         double division2 = 5.0 / 2;
         System.out.println (division2);
+        
+        float puntoFlotante = 3.14f;
+
+        double salario = 1270.50;
+        int valor = (int) salario;
+        System.out.println(valor);
+
+        double valor1 = 0.2;
+        double valor2 = 0.1;
+        double total = valor1 + valor2;
+
+        System.out.println(total);
 	}
 	
 }
