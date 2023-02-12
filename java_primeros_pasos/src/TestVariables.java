6a408e181ba5737c63b229641c01fb91bf56bdb7
jgnarvaez <jgnarvaez@unicauca.edu.co>
2023-02-11 20:55:28 -0500

    Test variable tipo int

---
+++ java_primeros_pasos/src/TestVariables.java
@@ -0,0 +1,12 @@
+
+public class TestVariables {
+	
+	public static void main (String[] args) {
+		System.out.println("Hola Mundo");
+		
+		int edad = 37;
+		
+		System.out.println("La edad es "+ edad + ", Felicitaciones!");
+	}
+	
+}
