c62c75fdba0f68c61d2be9c7806593b5b0f58656
jgnarvaez <jgnarvaez@unicauca.edu.co>
2023-02-15 16:51:31 -0500

    Excepciones, stacktrace, try-catch, Multi-Catch

--- java-pila-ejecucion/src/Flujo.java
+++ java-pila-ejecucion/src/Flujo.java
@@ -16,8 +16,27 @@ public class Flujo {
 	
 	private static void metodo2( ){
 		System.out.println("Inicio metodo2");
+		
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
+			
+			try { // Intenta esto
+				
+				if (i == 3) {
+					int num = 0;
+					int resultado = i/num;
+					System.out.println(resultado);
+				}
+				
+				String test = null;
+				System.out.println(test.toString());
+				
+			} catch (ArithmeticException | NullPointerException exception) { // Atrapa el error
+				System.out.println("Atrapo Excepcion");
+				System.out.println(exception.getMessage());
+				exception.printStackTrace();
+			}
+			
 		}
 		System.out.println("Fin metodo2");
 	}
